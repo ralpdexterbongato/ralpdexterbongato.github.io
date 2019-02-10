3db66902570e3d4955dfd917bd5a25d2925480a3
Ralp Dexter Bongato <rdc2898@gmail.com>
2019-02-10 21:53:51 +0800

    added redirector

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<script type="text/javascript">
-  window.location.href= 'https://ralpdexterbongato.github.io/portfolio';
-</script>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <base href="/">
+    <title>redirecting</title>
+  </head>
+  <body>
+
+  </body>
+  <script type="text/javascript">
+    window.location.href= 'https://ralpdexterbongato.github.io/portfolio';
+  </script>
+</html>
